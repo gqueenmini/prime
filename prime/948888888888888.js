b5e92a1982a7b81a2fb169e4d25258e7ca69e1c9
gqueenmini <gostsuranga@gmail.com>
2025-10-08 11:57:01 +0530

    Create premium.json via bot

---
+++ prime/948888888888888.js
@@ -0,0 +1,23 @@
+{
+  "mode": "public",
+  "changeLang": "en",
+  "antiCall": "off",
+  "ownerNumber": "948888888888888",
+  "ownerName": "",
+  "ownerFrom": "",
+  "ownerAge": "",
+  "menuLogo": null,
+  "aliveLogo": null,
+  "ownerLogo": null,
+  "autoTyping": false,
+  "autoRecording": false,
+  "antiDelete": false,
+  "statusRead": false,
+  "statusReact": false,
+  "autoRead": false,
+  "cmdRead": false,
+  "autoBlock": false,
+  "autoVoice": false,
+  "autoContact": false,
+  "alwaysOnline": false
+}
