0f2ed0bfae9d58d6a6798f6a22ea9fd941229b75
gqueenmini <gostsuranga@gmail.com>
2025-10-19 01:33:21 +0530

    Create premium.json via bot

---
+++ prime/94703421269.js
@@ -0,0 +1,23 @@
+{
+  "mode": "private",
+  "changeLang": "en",
+  "antiCall": "off",
+  "ownerNumber": "94703421269",
+  "ownerName": "කවිෂ",
+  "ownerFrom": "රන්නපුර",
+  "ownerAge": "21",
+  "menuLogo": null,
+  "aliveLogo": null,
+  "ownerLogo": null,
+  "autoTyping": true,
+  "autoRecording": false,
+  "antiDelete": true,
+  "statusRead": true,
+  "statusReact": true,
+  "autoRead": true,
+  "cmdRead": true,
+  "autoBlock": true,
+  "autoVoice": true,
+  "autoContact": true,
+  "alwaysOnline": true
+}
